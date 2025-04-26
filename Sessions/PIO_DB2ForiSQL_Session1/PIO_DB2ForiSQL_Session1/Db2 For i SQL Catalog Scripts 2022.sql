af4a6ee2fe99c69e29eb5c61e790505ac76b4b42
PIO-YOGESHJAIMAN <81214951+PIO-YOGESHJAIMAN@users.noreply.github.com>
2025-04-26 23:37:45 +0530

    Add files via upload

---
+++ Sessions/PIO_DB2ForiSQL_Session1/PIO_DB2ForiSQL_Session1/Db2 For i SQL Catalog Scripts 2022.sql
@@ -0,0 +1,54 @@
+﻿-- DB2 For i SQL Catalog Training SQL Script 2022
+
+-- SQL Query to get the Procedure information inclinding the Long Name based on Object Name
+-- How to find the SQL Inforamtion
+SELECT substr(SPECIFIC_SCHEMA,1,10) as LIBRARY,
+substr(SPECIFIC_NAME,1,20) as OBJECT_NAME,     
+ ROUTINE_NAME as LONG_NAME,               
+ IN_PARMS, OUT_PARMS, INOUT_PARMS                                   
+FROM qsys2.sysprocs                            
+WHERE SPECIFIC_NAME = '?????'; 
+             
+
+-- where ????? = the SQL Procedure Object Name    
+
+
+-- SQL Query to get the SQL Table information inclinding the Long Name based on Object Name
+-- How to find the SQL Inforamtion
+SELECT SYSTEM_TABLE_SCHEMA, SYSTEM_TABLE_NAME, TABLE_NAME,      
+ TABLE_TYPE, COLUMN_COUNT, TABLE_TEXT 
+FROM qsys2.systables 
+WHERE SYSTEM_TABLE_NAME = '?????'; 
+
+-- where ????? = the SQL Table Object Name                                       
+
+
+-- SQL Query to get the SQL Function information inclinding the Long Name based on Object Name 
+-- How to find the SQL Inforamtion
+SELECT substr(SPECIFIC_SCHEMA,1,10) as LIBRARY, 
+ substr(SPECIFIC_NAME,1,20) as OBJECT, 
+ ROUTINE_NAME as LONG_NAME, SQL_PATH,      
+ IN_PARMS, IS_DETERMINISTIC 
+FROM qsys2.sysfuncs 
+WHERE SPECIFIC_NAME = '?????'  
+
+-- where ????? = the SQL Function Object Name       
+
+-- SQL Query to get SQL View Information
+-- How to find the SQL Inforamtion
+ SELECT SYSTEM_VIEW_SCHEMA, SYSTEM_VIEW_NAME, TABLE_NAME 
+ FROM qsys2.sysviews 
+ WHERE SYSTEM_VIEW_NAME = '?????'   
+
+
+-- where ????? = the SQL View Object Name  
+
+-- SQL Query to get sql external program information 
+-- How to find the IBMi Object Infomation
+SELECT substr(SPECIFIC_SCHEMA,1,10) as LIBRARY, substr(SPECIFIC_NAME,1,10) as OBJECT,
+ ROUTINE_NAME as LONG_NAME, ROUTINE_BODY  as TYPE, 
+ EXTERNAL_NAME as IBMi_OBJECT_INFO
+FROM qsys2.sysROUTINES 
+WHERE SPECIFIC_NAME = '?????'   
+
+-- where ????? = the SQL External Name   
---
+++ Sessions/PIO_DB2ForiSQL_Session2/PIO_DB2ForiSQL_Session2/SAS120M.sql
@@ -0,0 +1,54 @@
+﻿-- *------------------------------------------------------------------*
+-- *   THIS MATERIAL IS LICENSED VISION-C CONSULTING INC. (VCCI)      *
+-- *------------------------------------------------------------------*
+-- *  DESIGNED BY: Hiram Hubbard                                      *
+-- *                                                                  *
+-- *        Table: SAS120M.SQL                                        *
+-- *                                                                  *
+-- *      Purpose: SA - Sales Analysis Salesman Master Table          *
+-- *               Example Only                                       *
+-- *  Licensed Materials - Property of VCCI,  All rights reserved     *
+-- ********************************************************************
+-- *                      Mod ID                                      *
+-- *    Date    Initials  Number      Description                     *
+-- *  --------  --------  --------    ------------------------------- *
+-- * 01/2017    HLH                   Initial Write                   *
+-- ********************************************************************
+
+CREATE TABLE SA_SALESMAN_MASTER_SAS120M
+
+     FOR SYSTEM NAME SAS120M (
+
+  SA_SALESMAN_ID   FOR COLUMN SASALMID DECIMAL(4,0) NOT NULL DEFAULT 0,
+  SA_SALESMAN_NAME FOR COLUMN SASALNM  CHAR(30) NOT NULL DEFAULT ' ',
+
+--  *******************************************************************
+--  * CREATE CDC COLUMNS FOR CHANGE DATA CAPTURE                      *
+--  *******************************************************************
+    CREATEUSER VARCHAR(20) ALLOCATE(10) NOT NULL DEFAULT USER,
+    CREATETIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CHANGEJOB  VARCHAR(28) CCSID 37 GENERATED ALWAYS AS (QSYS2.JOB_NAME),
+    CHANGEUSER VARCHAR(18) CCSID 37 GENERATED ALWAYS AS (USER),
+    CHANGETIME TIMESTAMP NOT NULL
+     GENERATED ALWAYS FOR EACH ROW ON UPDATE AS ROW CHANGE TIMESTAMP,
+
+--  ******************************************************************
+--  * PRIMARY KEY                                                    *
+--  ******************************************************************
+  CONSTRAINT SA_SAS120M_PK PRIMARY KEY
+    (SA_SALESMAN_ID))
+
+ RCDFMT SAR120M;
+
+ LABEL ON TABLE SA_SALESMAN_MASTER_SAS120M
+   IS 'Sales Analysis Saleman Master Table';
+
+ LABEL ON COLUMN SA_SALESMAN_MASTER_SAS120M
+  (SA_SALESMAN_ID    TEXT IS 'Salesman ID',
+   SA_SALESMAN_NAME  TEXT IS 'Salesman Name',
+   CREATEUSER TEXT IS 'CREATE USER ID',
+   CREATETIME TEXT IS 'CREATE TIMESTAMP',
+   CHANGEJOB  TEXT IS 'CHANGE JOB',
+   CHANGEUSER TEXT IS 'CHANGE USER',
+   CHANGETIME TEXT IS 'CHANGE TIMESTAMP');
+                                              
